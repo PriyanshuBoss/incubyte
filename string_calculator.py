933c0952c5a140cde77e1299362fe820b5161e16
PriyanshuBoss <rastogi.fresh88@gmail.com>
2025-02-19 12:39:41 +0530

    Added support for newline as a delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -1,9 +1,11 @@
+import re
 
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    return sum(map(int, numbers.split(",")))
+    return sum(map(int, re.split(",|\n", numbers)))
 
 print(add(""))  # 0
 print(add("1"))  # 1
 print(add("1,5")) #6
+print(add("1\n2,3"))  # 6
