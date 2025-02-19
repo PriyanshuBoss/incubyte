72d184f5d022e688cb5e63a73f5d2253eaa0d836
PriyanshuBoss <rastogi.fresh88@gmail.com>
2025-02-19 12:38:14 +0530

    Updated add function to handle multiple comma-separated numbers

--- string_calculator.py
+++ string_calculator.py
@@ -1,13 +1,9 @@
 
-
-
 def add(numbers: str) -> int:
-    
     if not numbers:
         return 0
-    
-    return int(numbers)
-
+    return sum(map(int, numbers.split(",")))
 
 print(add(""))  # 0
 print(add("1"))  # 1
+print(add("1,5")) #6
